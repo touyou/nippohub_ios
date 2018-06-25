0d7b52c1fa37f44627e4dab1e9cc2ad2ea11a508
touyou <fujiyou1121@gmail.com>
2018-06-25 10:46:57 +0900

    feature: [IMO] RxでのTableView読み込みとフォルダ構成の変更
    fix some

---
+++ nippohub_ios/Views/GroupIndexTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  GroupIndexTableViewCell.swift
+//  nippohub_ios
+//
+//  Created by 藤井陽介 on 2018/06/25.
+//  Copyright © 2018 うーぴょん. All rights reserved.
+//
+
+import UIKit
+
+class GroupIndexTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
