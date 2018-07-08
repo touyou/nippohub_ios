d247f7af7bc0d6da8d542a2bb9e50751f518ca52
宇山 拓夢 <sasurai.usagi3@gmail.com>
2018-07-09 08:59:32 +0900

    fix: 整理

--- nippohub_ios/Scenes/GroupsViewController.swift
+++ nippohub_ios/Scenes/GroupsViewController.swift
@@ -12,28 +12,16 @@ import RxCocoa
 
 class GroupsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-    @IBOutlet weak var groupIndexTableView: UITableView!/* {
-
-        didSet {
-
-            // ここは便利Extensionを生やすともう少し簡単になる
-            groupIndexTableView.register(UINib(nibName: "GroupIndexTableViewCell", bundle: nil), forCellReuseIdentifier: "groupItem")
-            
-            // 一旦ここにまとめる（リファクタの時にいろいろ変える余地あり）
-            let groups = Observable<[Group]>.just([Group(name: "test1", description: "body1"), Group(name: "test2", description: "body2"), Group(name: "test3", description: "body3")])
-            groups.bind(to: groupIndexTableView.rx.items(cellIdentifier: "groupItem", cellType: GroupIndexTableViewCell.self)) { row, element, cell in
-                cell.nameLabel.text = element.name
-                cell.descriptionLabel.text = element.description
-            }.disposed(by: disposeBag)
-        }
-    }*/
+    @IBOutlet weak var groupIndexTableView: UITableView!
     var groups: [Group] = []
     private let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         groupIndexTableView.register(UINib(nibName: "GroupIndexTableViewCell", bundle: nil), forCellReuseIdentifier: "groupItem")
         self.groups = [Group(name: "test1", description: "body1"), Group(name: "test2", description: "body2"), Group(name: "test3", description: "body3")]
+        
         groupIndexTableView.delegate = self
         groupIndexTableView.dataSource = self
         groupIndexTableView.reloadData()
