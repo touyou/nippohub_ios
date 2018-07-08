06ed5eb0eb60b289f14bfec40c1357d6641eeccb
宇山 拓夢 <sasurai.usagi3@gmail.com>
2018-07-09 08:57:27 +0900

    fix: Rxやめた

--- nippohub_ios/Scenes/GroupsViewController.swift
+++ nippohub_ios/Scenes/GroupsViewController.swift
@@ -10,27 +10,46 @@ import Foundation
 import RxSwift
 import RxCocoa
 
-class GroupsViewController: UIViewController {
+class GroupsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-    @IBOutlet weak var groupIndexTableView: UITableView! {
+    @IBOutlet weak var groupIndexTableView: UITableView!/* {
 
         didSet {
 
             // ここは便利Extensionを生やすともう少し簡単になる
             groupIndexTableView.register(UINib(nibName: "GroupIndexTableViewCell", bundle: nil), forCellReuseIdentifier: "groupItem")
+            
             // 一旦ここにまとめる（リファクタの時にいろいろ変える余地あり）
             let groups = Observable<[Group]>.just([Group(name: "test1", description: "body1"), Group(name: "test2", description: "body2"), Group(name: "test3", description: "body3")])
             groups.bind(to: groupIndexTableView.rx.items(cellIdentifier: "groupItem", cellType: GroupIndexTableViewCell.self)) { row, element, cell in
-
                 cell.nameLabel.text = element.name
                 cell.descriptionLabel.text = element.description
             }.disposed(by: disposeBag)
         }
-    }
-
+    }*/
+    var groups: [Group] = []
     private let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        groupIndexTableView.register(UINib(nibName: "GroupIndexTableViewCell", bundle: nil), forCellReuseIdentifier: "groupItem")
+        self.groups = [Group(name: "test1", description: "body1"), Group(name: "test2", description: "body2"), Group(name: "test3", description: "body3")]
+        groupIndexTableView.delegate = self
+        groupIndexTableView.dataSource = self
+        groupIndexTableView.reloadData()
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return groups.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "groupItem") as! GroupIndexTableViewCell
+        let group = self.groups[indexPath.row]
+        
+        cell.nameLabel.text = group.name
+        cell.descriptionLabel.text = group.description
+        
+        return cell
     }
 }
