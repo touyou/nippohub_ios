d6f1deed69c27e2c6a7e4b257e6ac7757c654818
宇山 拓夢 <sasurai.usagi3@gmail.com>
2018-09-03 08:00:22 +0900

    feature: 日報作成JSON作成

---
+++ nippohub_ios/Scenes/PostDailyReportViewController.swift
@@ -0,0 +1,31 @@
+//
+//  PostDailyReportViewController.swift
+//  nippohub_ios
+//
+//  Created by うさきち on 2018/09/03.
+//  Copyright © 2018年 うーぴょん. All rights reserved.
+//
+
+import UIKit
+
+class PostDailyReportViewController: UIViewController {
+    @IBOutlet var titleForm: UITextField!
+    @IBOutlet var bodyForm: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction
+    func postDailyReport() {
+        let dailyReport = PostDailyReportJson(title: titleForm.text!, body: bodyForm.text!)
+        
+        PostDailyReportService.exec(dailyReport: dailyReport)
+    }
+}
