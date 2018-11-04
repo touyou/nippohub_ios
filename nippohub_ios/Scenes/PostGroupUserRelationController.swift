9cbdcc3700e60b94c168322f41683fdf1ae15d7c
宇山 拓夢 <sasurai.usagi3@gmail.com>
2018-11-05 07:52:41 +0900

    feature: 画面遷移まで実装

---
+++ nippohub_ios/Scenes/PostGroupUserRelationController.swift
@@ -0,0 +1,26 @@
+//
+//  PostGroupUserRelationController.swift
+//  nippohub_ios
+//
+//  Created by うさきち on 2018/11/05.
+//  Copyright © 2018 うーぴょん. All rights reserved.
+//
+
+import UIKit
+
+class PostGroupUserRelationController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction
+    func postGroupUserRelation() {
+        dismiss(animated: true, completion: nil)
+    }
+}
