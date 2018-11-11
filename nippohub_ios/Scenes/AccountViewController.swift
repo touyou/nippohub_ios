5cf4eb59a96958539d216c787eb97bcac6603c0a
宇山 拓夢 <sasurai.usagi3@gmail.com>
2018-11-12 07:32:39 +0900

    feature: Controllerとの紐付け

---
+++ nippohub_ios/Scenes/AccountViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AccountViewController.swift
+//  nippohub_ios
+//
+//  Created by うさきち on 2018/11/12.
+//  Copyright © 2018 うーぴょん. All rights reserved.
+//
+
+import UIKit
+
+class AccountViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
