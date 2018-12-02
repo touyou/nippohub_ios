1c33773977211589d59f5d4f7c3ba9d7a5233921
宇山 拓夢 <sasurai.usagi3@gmail.com>
2018-12-03 07:37:19 +0900

    feature: uid追加

--- nippohub_ios/Models/User.swift
+++ nippohub_ios/Models/User.swift
@@ -11,9 +11,11 @@ import Foundation
 class User {
     var nickname: String
     var email: String?
+    let uid: String
     
-    init(nickname: String, email: String?) {
+    init(nickname: String, email: String?, uid: String) {
         self.nickname = nickname
         self.email = email
+        self.uid = uid
     }
 }
