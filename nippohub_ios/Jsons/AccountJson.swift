8266866a7a4e01316d0d17c216ac507a5567dd0a
宇山 拓夢 <sasurai.usagi3@gmail.com>
2018-12-03 07:51:04 +0900

    fix: uid追加

--- nippohub_ios/Jsons/AccountJson.swift
+++ nippohub_ios/Jsons/AccountJson.swift
@@ -12,9 +12,9 @@ struct AccountJson: Codable {
     let id: Int
     let email: String
     let nickname: String
+    let uid: String
     
     func toDomainObject() -> Account {
-        // TODO: uid実装
-        return Account(id: id, email: email, nickname: nickname, uid: "")
+        return Account(id: id, email: email, nickname: nickname, uid: uid)
     }
 }
